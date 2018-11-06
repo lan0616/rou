b77eb179f317c6f878cac4175239f5d88dc86e9c
lan0616 <18678207372@163.com>
2018-11-06 18:54:15 +0800

    project项目

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Css from 'common/css/style.css';
+import Header from 'comp/layout/header.jsx';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header></Header>
+      </div>
+    );
+  }
+}
+
+export default App;
